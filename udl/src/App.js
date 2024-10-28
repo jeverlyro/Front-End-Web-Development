78e6c1d46db66c301efd511679b597b6352de054
jeverlyro <ikadektresnajeverly78@gmail.com>
2024-10-28 21:38:56 +0800

    Updated exercise 12

--- udl/src/App.js
+++ udl/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Footer from "./components/Footer";
+import Header from "./components/header.js";
+import Home from "./components/home.js";
+import Footer from "./components/footer.js";
 
 const App = () => {
   return (
